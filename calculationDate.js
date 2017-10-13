5c76a14b9e555442bd5a83b62b0559b606431f1d
1043328710 <1043328710@qq.com>
2017-10-14 00:10:37 +0800

    计算给定两段时间差
    计算给定两段时间差

---
+++ calculationDate.js
@@ -0,0 +1,10 @@
+/*计算给定两段时间差（天） 如 2017-1-1  2017-9-10*/
+
+function calculationDate(date1, date2){
+	date1 = new Date(date1);
+	date2 = new Date(date2);
+	var date = +(date1 - date2);
+	return date / (60 * 60 * 24 * 1000);
+}
+
+calculationDate('2017-9-10','2017-1-1')
